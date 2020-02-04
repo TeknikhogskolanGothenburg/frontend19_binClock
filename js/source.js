a01aaa88e67cdf68267f6541a4534954a1f329e6
Joakim Wassberg <joakim@arthead.se>
2020-02-04 11:14:30 +0100

    30 jan

--- js/source.js
+++ js/source.js
@@ -2,6 +2,39 @@ window.setInterval(function() {
     clock();
 }, 1000);
 
+document.addEventListener('keydown', toggleClocks);
+function toggleClocks(e) {
+    let decClock = document.getElementById("dec-clock");
+    if(e.code === 'KeyD') {
+        if(decClock.style.opacity === '0') {
+            decClock.style.opacity = '0.6';
+        }
+        else {
+            decClock.style.opacity = '0';
+        }
+    }
+    
+    let hexClock = document.getElementById("hex-clock");
+    if(e.code === 'KeyH') {
+        if(hexClock.style.opacity === '0') {
+            hexClock.style.opacity = '0.6';
+        }
+        else {
+            hexClock.style.opacity = '0';
+        }
+    }
+
+    let binClock = document.getElementById("bin-clock");
+    if(e.code === 'KeyB') {
+        if(binClock.style.opacity === '0') {
+            binClock.style.opacity = '0.6';
+        }
+        else {
+            binClock.style.opacity = '0';
+        }
+    }
+}
+
 function tick(item, _) {
     let [n, t] = item;
     let bits = Array.from(t).reverse();
@@ -35,8 +68,23 @@ function clock() {
     
     time.forEach(tick);
 
-
+    // Decimal Clock
     let dcDiv = document.getElementById("dc");
-
     dcDiv.innerHTML = `${hour}:${minute}:${second}`;
+
+    // Hexadecimal clock
+    
+    let hexHour = hour.toString(16).padStart(2, '0');
+    let hexMinute = minute.toString(16).padStart(2, '0');
+    let hexSecond = second.toString(16).padStart(2, '0');
+    let hcDiv = document.getElementById("hc");
+    hcDiv.innerHTML = `${hexHour}:${hexMinute}:${hexSecond}`;
+
+    // Binary clock
+    let binHour = parseInt(hour, 10).toString(2).padStart(6, '0');
+    let binMinute = parseInt(minute, 10).toString(2).padStart(6, '0');
+    let binSecond = parseInt(second, 10).toString(2).padStart(6, '0');
+    let bcDiv = document.getElementById("bc");
+    bcDiv.innerHTML = `${binHour}:${binMinute}:${binSecond}`;
+    
 }
